f0c9846233021305e72d1030a1da4ab7f153b6a7
livimadev <mjlinarezv@yahoo.com>
2025-10-18 09:13:23 -0500

    Modelo de datos actualizado

---
+++ src/main/java/com/health/model/Product.java
@@ -0,0 +1,32 @@
+package com.health.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idProduct;
+
+    private String name;
+    private String description;
+    private String presentation;
+    private Double unitPrice;
+    private Integer stock;
+    private LocalDate expired;
+    private String drugLaboraty;
+}
